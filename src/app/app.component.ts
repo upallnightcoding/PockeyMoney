3f4a9ff824c3215fda2a5278a67d58a192efa533
upallnightcoding <upallnightcoding@gmail.com>
2026-04-22 21:56:13 -0400

    Initial Test Code Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  //standalone:true,
+  imports: [
+    RouterOutlet, 
+    MatSlideToggleModule, 
+    MatButtonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
